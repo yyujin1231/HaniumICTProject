86d961b853360105b4ed796b1d3cc113e42e443a
Yujin Lee <leeyujin1231@duksung.ac.kr>
2024-03-19 18:26:41 +0900

    Add : JPA 회원가입 로그인

--- src/main/java/com/example/packpagedosirak/entity/User.java
+++ src/main/java/com/example/packpagedosirak/entity/User.java
@@ -1,4 +1,27 @@
 package com.example.packpagedosirak.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+
+    private String role;
+
+
+
 }
